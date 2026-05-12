e70c39fd0e3a125976befa5d56d627159410a417
therki <123902898+therki@users.noreply.github.com>
2026-05-12 20:39:39 +0200

    Modificar dto tareas

--- src/main/java/com/openwebinars/todo/dto/GetTaskDto.java
+++ src/main/java/com/openwebinars/todo/dto/GetTaskDto.java
@@ -1,16 +1,23 @@
 package com.openwebinars.todo.dto;
 
 import com.openwebinars.todo.model.Task;
+import com.openwebinars.todo.model.Task.Priority;
 import com.openwebinars.todo.users.NewUserResponse;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public record GetTaskDto(
         Long id,
         String title,
         String description,
+        boolean completed,
         LocalDateTime createdAt,
         LocalDateTime deadline,
+        Priority priority,
+        String categoryName,
+        Set<String> tags,
         NewUserResponse author
 ) {
 
@@ -19,8 +26,14 @@ public record GetTaskDto(
                 t.getId(),
                 t.getTitle(),
                 t.getDescription(),
+                t.isCompleted(),
                 t.getCreatedAt(),
-                t.getDeadLine(),
+                t.getDeadline().atStartOfDay(),
+                t.getPriority(),
+                (t.getCategory() != null) ? t.getCategory().getTitle() : "Sin categoría",
+                t.getTags().stream()
+                        .map(tag -> tag.getName())
+                        .collect(Collectors.toSet()),
                 NewUserResponse.of(t.getAuthor())
         );
     }
