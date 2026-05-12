fe0e9b4f66058484f6b4b2f885ad3d500a2ebe5b
therki <123902898+therki@users.noreply.github.com>
2026-05-12 21:59:48 +0200

    Actualizar configuracion

--- src/main/java/com/openwebinars/todo/security/SecurityConfig.java
+++ src/main/java/com/openwebinars/todo/security/SecurityConfig.java
@@ -6,10 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,19 +34,30 @@ public class SecurityConfig {
                     excep.accessDeniedHandler(accessDeniedHandler);
                     excep.authenticationEntryPoint(authenticationEntryPoint);
                 })
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/h2-console/**",
+                                "/favicon.ico").permitAll()
                         .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
+                        // Control por roles
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/gestor/**").hasAnyRole("ADMIN", "GESTOR") // El Gestor gestiona categorías
+                        .requestMatchers("/user/**").hasAnyRole("ADMIN", "GESTOR", "USUARIO")
+
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated()
                 );
 
-
-        http.csrf((csrf) -> {
-            csrf.disable();
-        });
         http.headers((headers) ->
                 headers.frameOptions((opts) -> opts.disable()));
         return http.build();
     }
+
+
 }
